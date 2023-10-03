dafb7e04af049289a54dcee6cd5a828ea8ba6b1f
Matilemak <matias.alcayagac@gmail.com>
2023-10-02 21:38:02 -0300

    Pre entrega 2 javascript, calculo de cuotas en base a productos seleccionados

--- main.js
+++ main.js
@@ -1,39 +1,99 @@
 
-let ingresarNombre;
-let ingresarApellido;
 
-function calculoCuotas(){
-    while (true){
+
+function calculoCuotas() {
+    let ingresarNombre;
+    let ingresarApellido;
+    let carrito = {};
+
+    while (true) {
         ingresarNombre = prompt("Ingrese su nombre");
-        if(ingresarNombre === ""){
+        if (ingresarNombre === "") {
             alert("Error: Ingrese su nombre");
-        }else{
-            while (true){
-                ingresarApellido = prompt("Ingrese su apellido");
-                if(ingresarApellido === ""){
-                    alert("Error: Ingrese su apellido");
-                }else{
-                    alert("Bienvenido/a " +ingresarNombre + " " +ingresarApellido);
-                    
-                while(true){
-                    let monto = parseFloat(prompt("Ingrese el monto a pagar"));
-                    let cuotas = parseInt(prompt("Desde 0 a 12 cuotas precio contado"));
-                    if(isNaN(monto) || monto <= 0){
-                        alert("Error: Ingrese un monto válido")
-                    }else{
-                        if(isNaN(cuotas) || cuotas < 0 || cuotas > 12){
-                            alert("Error: Ingresa un número entre 0 y 12")
-                        }else{
-                            let pagoMensual = monto / (cuotas || 1);
-                            alert("Monto total a pagar: $" + monto.toFixed(0))
-                            alert("Pago mensual del: $" + pagoMensual.toFixed(0))
-                            let continuar = confirm("¿Desea realizar otra operación?");
-                            if(!continuar){
-                                return;
-                                }               
+        } else {
+            ingresarApellido = prompt("Ingrese su apellido");
+            if (ingresarApellido === "") {
+                alert("Error: Ingrese su apellido");
+            } else {
+                alert("Bienvenido/a " + ingresarNombre + " " + ingresarApellido);
+
+                const productos = {
+                    "camiseta": 100,
+                    "pantalon": 150,
+                    "chaqueta": 200,
+                };
+
+                let agregarOtraOperacion = true;
+
+                while (agregarOtraOperacion) {
+                    let productosTexto = "Productos disponibles:\n";
+                    for (const producto in productos) {
+                        productosTexto += `${producto}: $${productos[producto]}\n`;
+                    }
+                    alert(productosTexto);
+
+                    let agregarOtroProducto = true;
+
+                    while (agregarOtroProducto) {
+                        const productoElegido = prompt("Ingrese el nombre del producto").toLowerCase();
+
+                        if (productoElegido in productos) {
+                            const cantidad = parseInt(prompt("Ingrese la cantidad"));
+
+                            if (!isNaN(cantidad) && cantidad > 0) {
+                                
+                                if (carrito[productoElegido]) {
+                                    carrito[productoElegido] += cantidad;
+                                } else {
+                                    carrito[productoElegido] = cantidad;
+                                }
+                                alert(`Producto ${productoElegido} agregado al carrito.`);
+                            } else {
+                                alert("Error: Cantidad inválida");
                             }
+                        } else {
+                            alert("Error: Producto no encontrado");
+                        }
+
+                        agregarOtroProducto = confirm("¿Desea agregar otro producto?");
+                    }
+
+                    
+                    let precioTotal = 0;
+                    let detalleProductos = "Detalle de productos:\n";
+                    for (const producto in carrito) {
+                        const productoLowerCase = producto.toLowerCase();
+                        if (productoLowerCase in productos) {
+                            const cantidad = carrito[producto];
+                            const precioUnitario = productos[productoLowerCase];
+                            const precioProducto = cantidad * precioUnitario;
+                            detalleProductos += `${cantidad} ${producto}: $${precioProducto}\n`;
+                            precioTotal += precioProducto;
                         }
                     }
+                    alert(`Precio total a pagar: $${precioTotal}\n${detalleProductos}`);
+
+                    let cuotas;
+
+                    while (true) {
+                        cuotas = parseInt(prompt("Desde 0 a 12 cuotas precio contado"));
+                        if (!isNaN(cuotas) && cuotas >= 0 && cuotas <= 12) {
+                            break; 
+                        }
+                        alert("Error: Ingresa un número entre 0 y 12");
+                    }
+
+                    let pagoMensual = precioTotal / (cuotas || 1);
+                    alert("Pago mensual del: $" + pagoMensual.toFixed(0));
+                    agregarOtraOperacion = confirm("¿Desea realizar otra operación?");
+                    
+                    
+                    carrito = {};
+                }
+
+                let continuar = confirm("¿Desea realizar otra operación?");
+                if (!continuar) {
+                    return;
                 }
             }
         }
