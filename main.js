19d2cf8b25b53feb1782f4d534dcb7a7d079e826
Matilemak <matias.alcayagac@gmail.com>
2023-10-18 17:54:50 -0300

    avance pre entrega 3

--- main.js
+++ main.js
@@ -1,103 +1,101 @@
 
+// function calculoCuotas() {
+//     let ingresarNombre;
+//     let ingresarApellido;
+//     let carrito = {};
 
+//     while (true) {
+//         ingresarNombre = prompt("Ingrese su nombre");
+//         if (ingresarNombre === "") {
+//             alert("Error: Ingrese su nombre");
+//         } else {
+//             ingresarApellido = prompt("Ingrese su apellido");
+//             if (ingresarApellido === "") {
+//                 alert("Error: Ingrese su apellido");
+//             } else {
+//                 alert("Bienvenido/a " + ingresarNombre + " " + ingresarApellido);
 
-function calculoCuotas() {
-    let ingresarNombre;
-    let ingresarApellido;
-    let carrito = {};
+//                 const productos = {
+//                     "camiseta": 100,
+//                     "pantalon": 150,
+//                     "chaqueta": 200,
+//                 };
 
-    while (true) {
-        ingresarNombre = prompt("Ingrese su nombre");
-        if (ingresarNombre === "") {
-            alert("Error: Ingrese su nombre");
-        } else {
-            ingresarApellido = prompt("Ingrese su apellido");
-            if (ingresarApellido === "") {
-                alert("Error: Ingrese su apellido");
-            } else {
-                alert("Bienvenido/a " + ingresarNombre + " " + ingresarApellido);
+//                 let agregarOtraOperacion = true;
 
-                const productos = {
-                    "camiseta": 100,
-                    "pantalon": 150,
-                    "chaqueta": 200,
-                };
+//                 while (agregarOtraOperacion) {
+//                     let productosTexto = "Productos disponibles:\n";
+//                     for (const producto in productos) {
+//                         productosTexto += `${producto}: $${productos[producto]}\n`;
+//                     }
+//                     alert(productosTexto);
 
-                let agregarOtraOperacion = true;
+//                     let agregarOtroProducto = true;
 
-                while (agregarOtraOperacion) {
-                    let productosTexto = "Productos disponibles:\n";
-                    for (const producto in productos) {
-                        productosTexto += `${producto}: $${productos[producto]}\n`;
-                    }
-                    alert(productosTexto);
+//                     while (agregarOtroProducto) {
+//                         const productoElegido = prompt("Ingrese el nombre del producto").toLowerCase();
 
-                    let agregarOtroProducto = true;
+//                         if (productoElegido in productos) {
+//                             const cantidad = parseInt(prompt("Ingrese la cantidad"));
 
-                    while (agregarOtroProducto) {
-                        const productoElegido = prompt("Ingrese el nombre del producto").toLowerCase();
-
-                        if (productoElegido in productos) {
-                            const cantidad = parseInt(prompt("Ingrese la cantidad"));
-
-                            if (!isNaN(cantidad) && cantidad > 0) {
+//                             if (!isNaN(cantidad) && cantidad > 0) {
                                 
-                                if (carrito[productoElegido]) {
-                                    carrito[productoElegido] += cantidad;
-                                } else {
-                                    carrito[productoElegido] = cantidad;
-                                }
-                                alert(`Producto ${productoElegido} agregado al carrito.`);
-                            } else {
-                                alert("Error: Cantidad inválida");
-                            }
-                        } else {
-                            alert("Error: Producto no encontrado");
-                        }
+//                                 if (carrito[productoElegido]) {
+//                                     carrito[productoElegido] += cantidad;
+//                                 } else {
+//                                     carrito[productoElegido] = cantidad;
+//                                 }
+//                                 alert(`Producto ${productoElegido} agregado al carrito.`);
+//                             } else {
+//                                 alert("Error: Cantidad inválida");
+//                             }
+//                         } else {
+//                             alert("Error: Producto no encontrado");
+//                         }
 
-                        agregarOtroProducto = confirm("¿Desea agregar otro producto?");
-                    }
+//                         agregarOtroProducto = confirm("¿Desea agregar otro producto?");
+//                     }
 
                     
-                    let precioTotal = 0;
-                    let detalleProductos = "Detalle de productos:\n";
-                    for (const producto in carrito) {
-                        const productoLowerCase = producto.toLowerCase();
-                        if (productoLowerCase in productos) {
-                            const cantidad = carrito[producto];
-                            const precioUnitario = productos[productoLowerCase];
-                            const precioProducto = cantidad * precioUnitario;
-                            detalleProductos += `${cantidad} ${producto}: $${precioProducto}\n`;
-                            precioTotal += precioProducto;
-                        }
-                    }
-                    alert(`Precio total a pagar: $${precioTotal}\n${detalleProductos}`);
+//                     let precioTotal = 0;
+//                     let detalleProductos = "Detalle de productos:\n";
+//                     for (const producto in carrito) {
+//                         const productoLowerCase = producto.toLowerCase();
+//                         if (productoLowerCase in productos) {
+//                             const cantidad = carrito[producto];
+//                             const precioUnitario = productos[productoLowerCase];
+//                             const precioProducto = cantidad * precioUnitario;
+//                             detalleProductos += `${cantidad} ${producto}: $${precioProducto}\n`;
+//                             precioTotal += precioProducto;
+//                         }
+//                     }
+//                     alert(`Precio total a pagar: $${precioTotal}\n${detalleProductos}`);
 
-                    let cuotas;
+//                     let cuotas;
 
-                    while (true) {
-                        cuotas = parseInt(prompt("Desde 0 a 12 cuotas precio contado"));
-                        if (!isNaN(cuotas) && cuotas >= 0 && cuotas <= 12) {
-                            break; 
-                        }
-                        alert("Error: Ingresa un número entre 0 y 12");
-                    }
+//                     while (true) {
+//                         cuotas = parseInt(prompt("Desde 0 a 12 cuotas precio contado"));
+//                         if (!isNaN(cuotas) && cuotas >= 0 && cuotas <= 12) {
+//                             break; 
+//                         }
+//                         alert("Error: Ingresa un número entre 0 y 12");
+//                     }
 
-                    let pagoMensual = precioTotal / (cuotas || 1);
-                    alert("Pago mensual del: $" + pagoMensual.toFixed(0));
-                    agregarOtraOperacion = confirm("¿Desea realizar otra operación?");
+//                     let pagoMensual = precioTotal / (cuotas || 1);
+//                     alert("Pago mensual del: $" + pagoMensual.toFixed(0));
+//                     agregarOtraOperacion = confirm("¿Desea realizar otra operación?");
                     
                     
-                    carrito = {};
-                }
+//                     carrito = {};
+//                 }
 
-                let continuar = confirm("¿Desea realizar otra operación?");
-                if (!continuar) {
-                    return;
-                }
-            }
-        }
-    }
-}
+//                 let continuar = confirm("¿Desea realizar otra operación?");
+//                 if (!continuar) {
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+// }
 
-calculoCuotas();
+// calculoCuotas();
